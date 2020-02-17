cf14918965008d0329ffd1bba76e76631439be5b
Simon Chammah <simon.chammah@autodesk.com>
2020-02-17 11:55:39 +0200

    adjusted versions required

--- versions.tf
+++ versions.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.12.0"
 
   required_providers {
     aws = {
-      version = "~> 2.49.0"
+      version = ">= 2.28.0"
     }
   }
 }
