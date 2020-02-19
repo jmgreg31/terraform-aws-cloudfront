8136d13632007fad442410218639337d7a5e844c
Jonathan Greg <melissabailey@melissas-mbp-2.lan>
2020-02-18 20:28:15 -0500

    adding additional tags and outputs to the example

--- example/main.tf
+++ example/main.tf
@@ -10,6 +10,12 @@ variable acm_certificate_arn {
   default     = null
 }
 
+variable additional_tags {
+  description = "A mapping of additional tags to attach"
+  type        = map(string)
+  default     = {}
+}
+
 variable alias {
   description = "Aliases, or CNAMES, for the distribution"
   type        = list
@@ -135,6 +141,61 @@ variable webacl {
   default     = ""
 }
 
+output "id" {
+   value       = module.demo_cf.id
+   description = "The identifier for the distribution. For example: EDFDVBD632BHDS5."
+ }
+
+ output "arn" {
+   value       = module.demo_cf.arn
+   description = "The ARN (Amazon Resource Name) for the distribution. For example: arn:aws:cloudfront::123456789012:distribution/EDFDVBD632BHDS5, where 123456789012 is your AWS account ID."
+ }
+
+ output "caller_reference" {
+   value       = module.demo_cf.caller_reference
+   description = "Internal value used by CloudFront to allow future updates to the distribution configuration."
+ }
+
+ output "status" {
+   value       = module.demo_cf.status
+   description = "The current status of the distribution. Deployed if the distribution's information is fully propagated throughout the Amazon CloudFront system."
+ }
+
+ output "active_trusted_signers" {
+   value       = module.demo_cf.active_trusted_signers
+   description = "The key pair IDs that CloudFront is aware of for each trusted signer, if the distribution is set up to serve private content with signed URLs."
+ }
+
+ output "domain_name" {
+   value       = module.demo_cf.domain_name
+   description = "The domain name corresponding to the distribution. For example: d604721fxaaqy9.cloudfront.net."
+ }
+
+ output "name" {
+   value       = module.demo_cf.domain_name
+   description = "The domain name corresponding to the distribution. For example: d604721fxaaqy9.cloudfront.net."
+ }
+
+ output "last_modified_time" {
+   value       = module.demo_cf.last_modified_time
+   description = "The date and time the distribution was last modified."
+ }
+
+ output "in_progress_validation_batches" {
+   value       = module.demo_cf.in_progress_validation_batches
+   description = "The number of invalidation batches currently in progress."
+ }
+
+ output "etag" {
+   value       = module.demo_cf.etag
+   description = "The current version of the distribution's information. For example: E2QWRUHAPOMQZL."
+ }
+
+ output "hosted_zone_id" {
+   value       = module.demo_cf.hosted_zone_id
+   description = "The CloudFront Route 53 zone ID that can be used to route an Alias Resource Record Set to. This attribute is simply an alias for the zone ID Z2FDTNDATAQYW2."
+ }
+
 terraform {
   backend "s3" {
     bucket  = "jmgreg31"
@@ -153,6 +214,7 @@ module demo_cf {
   source                         = "git::https://github.com/jmgreg31/terraform-aws-cloudfront.git?ref=staging"
   create_cf                      = var.create_cf
   acm_certificate_arn            = var.acm_certificate_arn
+  additional_tags                = var.additional_tags
   alias                          = var.alias
   cloudfront_default_certificate = var.cloudfront_default_certificate
   comment                        = var.comment
