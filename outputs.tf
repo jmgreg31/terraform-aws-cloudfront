8eebea4542be5e6c8ab197d9db5bb3b7627c4350
Simon Chammah <simon.chammah@autodesk.com>
2020-08-25 13:32:44 +0300

    adjusted provider requirements

--- outputs.tf
+++ outputs.tf
@@ -19,7 +19,7 @@ output "status" {
 }
 
 output "trusted_signers" {
-  value       = length(aws_cloudfront_distribution.cloudfront_distribution) > 0 ? aws_cloudfront_distribution.cloudfront_distribution[0].trusted_signers : {}
+  value       = length(aws_cloudfront_distribution.cloudfront_distribution) > 0 ? aws_cloudfront_distribution.cloudfront_distribution[0].trusted_signers : []
   description = "The key pair IDs that CloudFront is aware of for each trusted signer, if the distribution is set up to serve private content with signed URLs."
 }
 
