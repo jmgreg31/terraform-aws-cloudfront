d3e95fd9fc5cf37f805a0872451b3abd90f6d9cf
Simon Chammah <simon.chammah@autodesk.com>
2020-08-25 13:34:00 +0300

    updated documentation to explain support of TLSv1.2_2019 policy

--- variables.tf
+++ variables.tf
@@ -120,7 +120,7 @@ variable iam_certificate_id {
 variable minimum_protocol_version {
   description = <<EOF
     The minimum version of the SSL protocol that you want CloudFront to use for HTTPS connections. 
-    One of SSLv3, TLSv1, TLSv1_2016, TLSv1.1_2016 or TLSv1.2_2018. Default: TLSv1. 
+    One of SSLv3, TLSv1, TLSv1_2016, TLSv1.1_2016,TLSv1.2_2018 or TLSv1.2_2019. Default: TLSv1. 
     NOTE: If you are using a custom certificate (specified with acm_certificate_arn or iam_certificate_id), 
     and have specified sni-only in ssl_support_method, TLSv1 or later must be specified. 
     If you have specified vip in ssl_support_method, only SSLv3 or TLSv1 can be specified. 
