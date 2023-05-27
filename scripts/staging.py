10d1efb2ac307aae0904d9d0b3e164719a01b692
Jonathan Greg <jmgreg31@gmail.com>
2023-05-27 09:09:31 -0500

    staging scripts

--- scripts/staging.py
+++ scripts/staging.py
@@ -13,9 +13,9 @@ def update_file(path: str, expression: str, replacement: str) -> None:
 
 
 def update_example() -> None:
-    replacement = "source = ../"
+    replacement = 'source = "../"'
     update_file(f"{WORK_DIR}/example/main.tf", r"source[ \t]+\=.*", replacement)
-    os.system("./terraform fmt example/")
+    os.system("terraform fmt example/")
 
 
 if __name__ == "__main__":
