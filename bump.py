0fa2a974ee79c84d4ceb9f4defadb61406d0d4d5
Jonathan Greg <jmgreg31@gmail.com>
2023-05-27 08:25:04 -0500

    update

--- bump.py
+++ bump.py
@@ -1,18 +1,5 @@
-import logging
 import os
 import re
-
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
-
-
-def should_bump() -> bool:
-    title = os.getenv("PR_TITLE")
-    logger.debug(f"PR TITLE: {title}")
-    if "skipversion" in title:
-        logger.info("Skipping Version Bump")
-        return False
-    return True
 
 
 def get_version() -> str:
@@ -74,8 +61,7 @@ def push_changes() -> None:
 
 
 if __name__ == "__main__":
-    if should_bump():
-        update_readme()
-        update_changelog()
-        update_example()
-        push_changes()
+    update_readme()
+    update_changelog()
+    update_example()
+    push_changes()
