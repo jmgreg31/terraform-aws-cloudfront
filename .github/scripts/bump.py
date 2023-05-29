c1352e4b1ffc49acfcdf3de2af35eae16fb2d6bc
Jonathan Greg <jmgreg31@gmail.com>
2023-05-29 00:07:15 -0400

    test[norelease]

--- .github/scripts/bump.py
+++ .github/scripts/bump.py
@@ -55,27 +55,46 @@ class BumpHandler(FileHandler):
         return file_changes
 
     def push_file_changes(self) -> None:
-        os.system(f"{WORK_DIR}/terraform fmt {WORK_DIR}/example/ > /dev/null 2>&1")
-        os.system('git config --global user.email "jmgreg31@gmail.com"')
-        os.system('git config --global user.name "Jon Greg"')
-        os.system("git config --global init.defaultBranch master")
-        os.system(f"cd {WORK_DIR} && git init")
-        os.system(
-            f"cd {WORK_DIR} && git remote add origin https://jmgreg31:{TOKEN}@github.com/{ORG}/{REPO}.git > /dev/null 2>&1"
-        )
-        # os.system("git checkout master")
-        LOG.info("add")
-        os.system(f"cd {WORK_DIR} && git add README.md")
-        os.system(f"cd {WORK_DIR} && git add CHANGELOG.md")
-        os.system(f"cd {WORK_DIR} && git add example/main.tf")
-        os.system(f"cd {WORK_DIR} && git add example/terraform.tfvars")
-        os.system(
-            f'cd {WORK_DIR} && git commit -m "(ci): Bump Version to {self.version}"'
-        )
-        if not self.dry_run:
-            os.system(f"cd {WORK_DIR} && git push origin master")
-        else:
-            LOG.info(f"Version {self.version} is the latest release")
+        with os.chdir(WORK_DIR):
+            os.system("terraform fmt example/ > /dev/null 2>&1")
+            os.system('git config --global user.email "jmgreg31@gmail.com"')
+            os.system('git config --global user.name "Jon Greg"')
+            os.system("git config --global init.defaultBranch master")
+            os.system(f"git init")
+            os.system(
+                f"git remote add origin https://jmgreg31:{TOKEN}@github.com/{ORG}/{REPO}.git > /dev/null 2>&1"
+            )
+            # os.system("git checkout master")
+            os.system("git add README.md")
+            os.system("git add CHANGELOG.md")
+            os.system("git add example/main.tf")
+            os.system("git add example/terraform.tfvars")
+            os.system(f'git commit -m "(ci): Bump Version to {self.version}"')
+            if not self.dry_run:
+                os.system("git push origin master")
+            else:
+                LOG.info(f"Version {self.version} is the latest release")
+
+            # os.system(f"{WORK_DIR}/terraform fmt {WORK_DIR}/example/ > /dev/null 2>&1")
+            # os.system('git config --global user.email "jmgreg31@gmail.com"')
+            # os.system('git config --global user.name "Jon Greg"')
+            # os.system("git config --global init.defaultBranch master")
+            # os.system(f"cd {WORK_DIR} && git init")
+            # os.system(
+            #     f"cd {WORK_DIR} && git remote add origin https://jmgreg31:{TOKEN}@github.com/{ORG}/{REPO}.git > /dev/null 2>&1"
+            # )
+            # # os.system("git checkout master")
+            # os.system(f"cd {WORK_DIR} && git add README.md")
+            # os.system(f"cd {WORK_DIR} && git add CHANGELOG.md")
+            # os.system(f"cd {WORK_DIR} && git add example/main.tf")
+            # os.system(f"cd {WORK_DIR} && git add example/terraform.tfvars")
+            # os.system(
+            #     f'cd {WORK_DIR} && git commit -m "(ci): Bump Version to {self.version}"'
+            # )
+            # if not self.dry_run:
+            #     os.system(f"cd {WORK_DIR} && git push origin master")
+            # else:
+            #     LOG.info(f"Version {self.version} is the latest release")
 
 
 def main():
